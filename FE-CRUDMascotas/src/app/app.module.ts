e86f3e7fa812e6abe227a917b06011811dcd4073
AVANADE-CORP\antonio.gomez.guzman <antonio.gomez.guzman@avanade.com>
2024-04-23 11:44:04 +0200

    Boton Aceptar agregado

--- FE-CRUDMascotas/src/app/app.module.ts
+++ FE-CRUDMascotas/src/app/app.module.ts
@@ -18,6 +18,8 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatIconModule} from '@angular/material/icon';
 import {MatTooltipModule} from '@angular/material/tooltip';
+import {MatButtonModule} from '@angular/material/button';
+
 
 
 
@@ -39,7 +41,8 @@ import {MatTooltipModule} from '@angular/material/tooltip';
     MatFormFieldModule,
     MatInputModule,
     MatIconModule,
-    MatTooltipModule
+    MatTooltipModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
