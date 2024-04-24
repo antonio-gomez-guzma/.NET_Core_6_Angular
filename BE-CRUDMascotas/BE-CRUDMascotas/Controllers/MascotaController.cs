7bab67eb8972b1c9caa174d1b89cc4bdb9a77668
AVANADE-CORP\antonio.gomez.guzman <antonio.gomez.guzman@avanade.com>
2024-04-24 11:45:39 +0200

    Controlador y Endpoint creados

---
+++ BE-CRUDMascotas/BE-CRUDMascotas/Controllers/MascotaController.cs
@@ -0,0 +1,35 @@
+﻿using BE_CRUDMascotas.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.EntityFrameworkCore;
+
+namespace BE_CRUDMascotas.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MascotaController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MascotaController(ApplicationDbContext context)
+        {
+            _context = context;
+           
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                var listaMascotas = await _context.Mascotas.ToListAsync();
+                return Ok(listaMascotas);
+            }catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+    }
+}
