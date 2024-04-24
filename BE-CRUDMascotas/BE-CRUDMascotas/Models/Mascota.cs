85885df3cc8d5a47ebc449a7a550173715666319
AVANADE-CORP\antonio.gomez.guzman <antonio.gomez.guzman@avanade.com>
2024-04-24 11:27:42 +0200

    Creación BackEnd y base de datos

---
+++ BE-CRUDMascotas/BE-CRUDMascotas/Models/Mascota.cs
@@ -0,0 +1,12 @@
+﻿namespace BE_CRUDMascotas.Models
+{
+    public class Mascota
+    {
+        public int Id { get; set; }
+        public string Raza { get; set; }
+        public string Color { get; set; }
+        public int Edad { get; set; }
+        public float Peso { get; set; }
+        public DateTime FechaCreacion { get; set; } 
+    }
+}
