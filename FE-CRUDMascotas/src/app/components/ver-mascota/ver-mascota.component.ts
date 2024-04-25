a7a77e6ffab8000232fa2e5e7465b6d8c2e6a347
AVANADE-CORP\antonio.gomez.guzman <antonio.gomez.guzman@avanade.com>
2024-04-25 08:45:01 +0200

    Barra de loading implementada para la visualización de los datos de mascota

--- FE-CRUDMascotas/src/app/components/ver-mascota/ver-mascota.component.ts
+++ FE-CRUDMascotas/src/app/components/ver-mascota/ver-mascota.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 import { Mascota } from 'src/app/interface/mascota';
 import { MascotaService } from 'src/app/services/mascota.service';
 
@@ -12,6 +13,9 @@ export class VerMascotaComponent implements OnInit {
 
   id: number;
   mascota!: Mascota; 
+  loading: boolean = false;
+
+  // mascota$!: Observable<Mascota> //Que la variable termine con $ significa que es un observable, es un convenio // PIPE ASYNC
 
   constructor(private _mascotaService: MascotaService,
     private aRoute: ActivatedRoute
@@ -22,12 +26,15 @@ export class VerMascotaComponent implements OnInit {
 
   ngOnInit(): void {
     this.obtenerMascota()
+    //this.mascota$ = this._mascotaService.getMascota(this.id) // PIPE ASYNC
   }
 
   obtenerMascota()
   {
+    this.loading = true;
     this._mascotaService.getMascota(this.id).subscribe(data =>
       {
+        this.loading = false;
         this.mascota = data;
       }
     )
