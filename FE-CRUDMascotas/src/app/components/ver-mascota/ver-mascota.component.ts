7e046164f3e30509fef10e0f937d029f66aea982
AVANADE-CORP\antonio.gomez.guzman <antonio.gomez.guzman@avanade.com>
2024-04-25 08:34:16 +0200

    Visualización de mascota en frontEnd con los datos del BackEnd

--- FE-CRUDMascotas/src/app/components/ver-mascota/ver-mascota.component.ts
+++ FE-CRUDMascotas/src/app/components/ver-mascota/ver-mascota.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Mascota } from 'src/app/interface/mascota';
+import { MascotaService } from 'src/app/services/mascota.service';
 
 @Component({
   selector: 'app-ver-mascota',
@@ -7,9 +10,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VerMascotaComponent implements OnInit {
 
-  constructor() { }
+  id: number;
+  mascota!: Mascota; 
 
-  ngOnInit(): void {
+  constructor(private _mascotaService: MascotaService,
+    private aRoute: ActivatedRoute
+  ) 
+  {
+    this.id = +this.aRoute.snapshot.paramMap.get('id')!; // Con el + se transforma a int, la ! es para decir que puede ser nulo.
   }
 
+  ngOnInit(): void {
+    this.obtenerMascota()
+  }
+
+  obtenerMascota()
+  {
+    this._mascotaService.getMascota(this.id).subscribe(data =>
+      {
+        this.mascota = data;
+      }
+    )
+  }
 }
